59a0dbfb0c0d8826181c6f2978206dca84d7bd1e
srushtigosai123 <105599188+srushtigosai123@users.noreply.github.com>
2022-05-15 23:13:44 +0530

    Update settings.py

--- tecom/settings.py
+++ tecom/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '0z9+&_%1-8rs$eepw!vhq0sd-rbnnhcstm)av#+wdc$+0n=&b&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['srushtigosai123.pythonanywhere.com', '127.0.0.1:8000',]
 
 
 # Application definition
